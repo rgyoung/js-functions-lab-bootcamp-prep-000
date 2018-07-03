e74331fc1f3a30e249dad13c1150d3c520410233
youngrob84@yahoo.com <youngrob84@yahoo.com>
2018-07-03 14:32:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidays () {
   return "Happy holidays!"
 }
-function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(string){
+  return "Happy holidays, ${string}!"
 }
